6d2194f92f40efeb5821c075f439155c145dd19d
Saabir <82912568+ProdiGeeZ@users.noreply.github.com>
2023-12-18 21:57:13 +0000

    Second commit

--- member.js
+++ member.js
@@ -1,9 +1,3 @@
-function skillsMember() {
-    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
-    var member = {
-        name: "Nguyen Van A",
-        age: 20,
-        skills: skills
-    }
-    return member;
+function skillMember(){
+    console.log('skillMember');
 }
